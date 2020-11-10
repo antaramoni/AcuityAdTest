bce9baf884854fb295d17175f00b8283c9d3fcc2
Antara Chowdhury <antara.chowdhury@redspace.com>
2020-11-10 10:50:42 -0400

    Made changes into selenium test

--- src/test/seleniumtest/HomeHeaderTest.java
+++ src/test/seleniumtest/HomeHeaderTest.java
@@ -1,7 +1,9 @@
 package seleniumtest;
 
 import acuity.pages.HomePage;
+import org.junit.Assert;
 import org.junit.Test;
+import org.testng.asserts.IAssert;
 
 public class HomeHeaderTest extends TestInitialize {
 
@@ -9,23 +11,41 @@ public class HomeHeaderTest extends TestInitialize {
     public void HomeHeader() throws InterruptedException {
 
         HomePage homepage = new HomePage();
+      try {
+          // Validate header has Solutions link
+          assert (homepage.linkSolutions.getText().equals("Solutions"));
+          System.out.println(homepage.linkSolutions.getText());
+      }
+      catch (Exception e){
+          System.out.println("Solutions link  Validation:"+e);
+      }
 
-        // Validate header has Solutions link
-        homepage.linkSolutions.getText().equals("Solutions");
-        System.out.println(homepage.linkSolutions.getText());
+      try {
+          // Validate header has Insights link
+          assert (homepage.linkInsights.getText().equals("Insights"));
+          System.out.println(homepage.linkInsights.getText());
+      }
+      catch (Exception e){
+          System.out.println("Insights link Validation:"+e);
+      }
 
-        // Validate header has Insights link
-        homepage.linkInsights.getText().equals("Insights");
-        System.out.println(homepage.linkInsights.getText());
+      try {
+          // Validate header has News link
+          assert (homepage.linkNews.getText().equals("News"));
+          System.out.println(homepage.linkNews.getText());
+      }
+      catch (Exception e){
+          System.out.println("News link Validation:"+e);
+      }
 
-        // Validate header has News link
-        homepage.linkNews.getText().equals("News");
-        System.out.println(homepage.linkNews.getText());
-
-        // Validate header has Company link
-        homepage.linkCompany.getText().equals("Company");
-        System.out.println(homepage.linkCompany.getText());
-
+      try {
+          // Validate header has Company link
+          assert (homepage.linkCompany.getText().equals("Company"));
+          System.out.println(homepage.linkCompany.getText());
+      }
+      catch (Exception e){
+          System.out.println("Company links  Validation:"+e);
+      }
 
     }
 
